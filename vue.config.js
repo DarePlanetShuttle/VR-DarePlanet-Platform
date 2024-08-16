21f05739d4a751368fe06c4b2e161e0c7ebfd814
jaumecortes <jaume.cortes@dareplanet.com>
2024-08-16 15:16:11 +0200

    Dist folder

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   pages: {
     index: {
       // entry for the page
-      entry: 'src/index/main.js',
+      entry: 'src/main.js',
       // the source template
       template: 'public/index.html',
       // output as dist/index.html
@@ -18,11 +18,6 @@ module.exports = defineConfig({
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    // when using the entry-only string format,
-    // template is inferred to be `public/subpage.html`
-    // and falls back to `public/index.html` if not found.
-    // Output filename is inferred to be `subpage.html`.
-    subpage: 'src/subpage/main.js'
+    }
   }
 })
