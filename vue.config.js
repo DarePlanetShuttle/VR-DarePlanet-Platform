40d12a70cf58ffd1da052b9475360f2d87627650
Jose Antonio Labandibar <jose.labandibar@darecode.com>
2024-09-09 22:51:26 +0200

    Try 4

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,18 @@
 const { defineConfig } = require('@vue/cli-service');
-
-/*module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/VR-DarePlanet-Platform/'
-    : '/',
-});*/
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
+  chainWebpack: config => {
+    config.plugin('copy').tap(([options]) => {
+      options[0].patterns.push({
+        from: path.resolve(__dirname, '_redirects'), // Ruta de tu archivo _redirects en el proyecto
+        to: path.resolve(__dirname, 'dist'),        // Copia el archivo a la carpeta dist
+        toType: 'dir'
+      });
+      return [options];
+    });
+  }
 });
+
