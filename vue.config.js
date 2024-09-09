b3d21e498daa8dc67b98d21fc0d47ae1e8f209fa
Jose Antonio Labandibar <jose.labandibar@darecode.com>
2024-09-09 22:56:08 +0200

    Try 7

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
-const path = require('path');
+
+/*module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VR-DarePlanet-Platform/'
+    : '/',
+});*/
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
-  chainWebpack: config => {
-    config.plugin('copy').tap(([options]) => {
-      // Verificamos si 'patterns' existe. Si no, lo creamos como un array vacío.
-      if (!options[0].patterns) {
-        options[0].patterns = [];
-      }
-      
-      // Agregamos la nueva redirección a las patterns
-      options[0].patterns.push({
-        from: path.resolve(__dirname, '_redirects'),
-        to: path.resolve(__dirname, 'dist'),
-        toType: 'dir'
-      });
-
-      return [options];
-    });
-  }
 });
